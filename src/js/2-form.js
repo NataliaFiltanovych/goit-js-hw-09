a0983f0d368c0ac2f2b5d48f18880d854da3453e
Natalia_Filtanovych <natashe4ka21092016@gmail.com>
2026-03-03 23:45:01 +0200

    fix 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -18,11 +18,13 @@ form.addEventListener('input', e => {
 
 document.addEventListener('DOMContentLoaded', e => {
   const zip = localStorage.getItem('feedback-form-state');
-  const data = JSON.parse(zip) || formData;
-  form.elements.email.value = data.email || '';
-  form.elements.message.value = data.message || '';
-  formData.email = data.email;
-  formData.message = data.message;
+  if (zip !== '') {
+    const data = JSON.parse(zip) || formData;
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+    formData.email = data.email;
+    formData.message = data.message;
+  }
 });
 
 form.addEventListener('submit', e => {
