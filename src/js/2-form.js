140c0434e96f4c4c40f50c99209de0d8b3b266d7
Natalia_Filtanovych <natashe4ka21092016@gmail.com>
2026-03-03 22:17:51 +0200

    update 1-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const data = form.querySelector('.form-input');
+
+form.addEventListener('input', e => {
+  const formGet = new FormData(form);
+
+  const formData = {
+    email: formGet.get('email'),
+    message: formGet.get('message'),
+  };
+
+  const zip = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', zip);
+});
+
+document.addEventListener('DOMContentLoaded', e => {
+  const zip = localStorage.getItem('feedback-form-state');
+  const data = JSON.parse(zip);
+  form.elements.email.value = data.email;
+  form.elements.message.value = data.message;
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formGet = new FormData(form);
+
+  const formData = {
+    email: formGet.get('email'),
+    message: formGet.get('message'),
+  };
+
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+});
